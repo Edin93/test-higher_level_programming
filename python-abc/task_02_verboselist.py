6c065310e8158013f8e9ae6a2ec240bf70c9d476
Houssem Eddine BEN KHALIFA <il_matador@hotmail.fr>
2024-11-18 10:41:51 +0000

    task 2 done

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Python built-in classes extention module"""
+
+
+class VerboseList(list):
+    """A class built upon the list default class"""
+
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, items):
+        super().extend(items)
+        print(f"Extended the list with [{len(items)}] items.")
+
+    def remove(self, item):
+        if item in self:
+            print(f"Removed [{item}] from the list.")
+            super().remove(item)
+
+    def pop(self, position = -1):
+        print(f"Popped [{self[position]}] from the list.")
+        super().pop(position)
