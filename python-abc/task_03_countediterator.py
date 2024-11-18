32afd9cb2d2a90750432c967f4f087634d4cbd7f
Houssem Eddine BEN KHALIFA <il_matador@hotmail.fr>
2024-11-18 11:21:48 +0000

    task 3 done

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
-""""""
+"""Iter function extended class module"""
+
+
+class CountedIterator():
+    """Custom iterator class that counts the iterator items"""
+
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.counter = 0
+
+    def get_count(self):
+        return self.counter
+
+    def __next__(self):
+        self.counter += 1
+        try:
+            item = next(self.iterator)
+            return item
+        except StopIteration:
+            raise StopIteration
